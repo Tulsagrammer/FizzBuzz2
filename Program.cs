68509806d52880249321c3f5b3745f47a7801c47
Eric Chevalier <etech@tulsagrammer.com>
2015-02-19 12:25:28 -0600

    Added a second approach using indirect access to the tags.

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ containing NO conditionals.
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +26,7 @@ namespace FizzBuzz2
 {
     class Program
     {
-        private static readonly String[] Tags =
+        private static readonly string[] Tags =
         {
             @"FizzBuzz",
             @"Fizz",
@@ -44,12 +45,27 @@ namespace FizzBuzz2
             @""
         };
 
+        private static readonly byte[,] TagsIndex =
+        {
+            {3, 1, 1, 1, 1},        // V mod 3 = 0
+            {2, 0, 0, 0, 0},        // V mod 3 = 1
+            {2, 0, 0, 0, 0}         // V mod 3 = 2
+        };
+
+        private static readonly string[] Tags2 = {"", "Fizz", "Buzz", "FizzBuzz"};
+
+
         static void Main(string[] args)
         {
+            // Index directly into the "Tags" table.
             for (var i = 1; i <= 100; i++)
-            {
                 Console.WriteLine("{0,3} {1}", i, Tags[(i % 3) * 5 + i % 5]);
-            }
+
+            // Index into the "Tags2" table via the "TagsIndex" table.
+            // Inspired by a suggestion from Sean W.
+            Console.WriteLine();
+            for (var i = 1; i <= 100; i++)
+                Console.WriteLine("{0,3} {1}", i, Tags2[TagsIndex[i % 3, i % 5]]);
 
             Console.WriteLine();
             Console.Write(@"Press any key to continue...");
