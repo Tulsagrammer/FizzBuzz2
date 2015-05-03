6bb7173acc043a197515f4f04d08399a071d52a0
Eric Chevalier <etech@tulsagrammer.com>
2015-05-03 10:59:28 -0600

    Refactor #1: moved FizzBuzz logic to a separate class. Retained all Console I/O.

---
+++ FizzBuzz.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz2
+{
+    class FizzBuzz
+    {
+        private int _upperLimit;
+        private int _maxLoops;
+
+        private readonly List<Tuple<TimeSpan, string>>
+            _procTimes = new List<Tuple<TimeSpan, string>>();
+
+        public void Run(int upperLimit, int maxLoops)
+        {
+            _upperLimit = upperLimit;
+            _maxLoops   = maxLoops;
+
+            TestRunner(EricsFineSolution1, "EricsFineSolution1");
+            TestRunner(EricsFineSolution2, "EricsFineSolution2");
+            TestRunner(MildlyCleverSolution1, "MildlyCleverSolution1");
+            TestRunner(MildlyCleverSolution2, "MildlyCleverSolution2");
+            TestRunner(GrotesquelyOverengineeredSolution,
+                        "GrotesquelyOverengineeredSolution");
+
+            Console.WriteLine();
+            Console.Error.WriteLine(@"Results:");
+            _procTimes.ForEach(t => Console.Error.WriteLine(@"{0}  {1}", t.Item1, t.Item2));
+            Console.Error.WriteLine(@"Each test performed {0} times with max range of {1}.",
+                        _maxLoops, _upperLimit);
+        }
+
+        private void TestRunner(Action<int> testAction, string tag)
+        {
+            var start = Process.GetCurrentProcess().UserProcessorTime;
+            for (var i = 0; i < _maxLoops; i++)
+                testAction(_upperLimit);
+            var procTime = Process.GetCurrentProcess().UserProcessorTime.Subtract(start);
+            _procTimes.Add(Tuple.Create(procTime, tag));
+        }
+
+
+        #region Eric's Fine Solutions
+
+        private static readonly string[] Tags =
+        {
+            @"FizzBuzz", @"Fizz", @"Fizz", @"Fizz", @"Fizz",    // V mod 3 = 0
+            @"Buzz",     @"",     @"",     @"",     @"",        // V mod 3 = 1
+            @"Buzz",     @"",     @"",     @"",     @""         // V mod 3 = 2
+        };
+
+        private static readonly byte[,] TagsIndex =
+        {
+            {3, 1, 1, 1, 1},        // V mod 3 = 0
+            {2, 0, 0, 0, 0},        // V mod 3 = 1
+            {2, 0, 0, 0, 0}         // V mod 3 = 2
+        };
+
+        private static readonly string[] Tags2 = { "", "Fizz", "Buzz", "FizzBuzz" };
+
+        private static void EricsFineSolution1(int upperRange)
+        {
+            // Index directly into the "Tags" table.
+            for (var i = 1; i <= upperRange; i++)
+                Console.WriteLine("{0,3} {1}", i, Tags[(i % 3) * 5 + i % 5]);
+        }
+
+        private static void EricsFineSolution2(int upperRange)
+        {
+            // Index into the "Tags2" table via the "TagsIndex" table.
+            // Inspired by a suggestion from Sean W.
+            Console.WriteLine();
+            for (var i = 1; i <= upperRange; i++)
+                Console.WriteLine("{0,3} {1}", i, Tags2[TagsIndex[i % 3, i % 5]]);
+        }
+
+        #endregion
+
+        #region A Mildly-Clever Solution
+
+        private static readonly string[][] Fizzbuzz =
+        {
+            new[] { "FizzBuzz", "Fizz" },
+            new[] { "Buzz", "" }
+        };
+
+        private static void MildlyCleverSolution1(int upperRange)
+        {
+            // This is another alternative, suggested by Sean W.
+            Console.WriteLine();
+            for (var i = 1; i <= upperRange; ++i)
+            {
+                Fizzbuzz[1][1] = "" + i;
+                var f = (int)Math.Ceiling((i % 3) / (double)100);
+                var b = (int)Math.Ceiling((i % 5) / (double)100);
+                Console.WriteLine(i + ": " + Fizzbuzz[f][b]);
+            }
+
+        }
+
+        #endregion
+
+        #region A Modified Mildly-Clever Solution
+
+        private static readonly string[][] Fizzbuzz2 =
+        {
+            new[] { @"{0}: FizzBuzz", @"{0}: Fizz" },
+            new[] { @"{0}: Buzz",     @"{0}: {0}"  }
+        };
+
+        private static void MildlyCleverSolution2(int upperRange)
+        {
+            // This is another alternative, suggested by Sean W.
+            Console.WriteLine();
+            for (var i = 1; i <= upperRange; ++i)
+            {
+                var f = (int)Math.Ceiling((i % 3) / (double)100);
+                var b = (int)Math.Ceiling((i % 5) / (double)100);
+                Console.WriteLine(Fizzbuzz2[f][b], i);
+            }
+
+        }
+
+        #endregion
+
+        #region Grotesquely Over-Engineered Solution
+
+        private readonly string[] _textArray = { "Beer!!!", "Buzz", "Fizz", "" };
+
+        private void GrotesquelyOverengineeredSolution(int upperRange)
+        {
+            var foo = Enumerable.Range(1, upperRange);
+
+            Console.WriteLine();
+            foreach (var i in foo)
+            {
+                _textArray[3] = i.ToString(CultureInfo.InvariantCulture);
+                var a = GetArrayIndexes(i, upperRange);
+                PrintValue(GetText(a));
+            }
+        }
+
+        public Tuple<int, int> GetArrayIndexes(int i, int upperRange)
+        {
+            var a = (int)Math.Ceiling((double)i % 3 / 100);
+            var b = (int)Math.Ceiling((double)i % 5 / 100) * 2;
+            return new Tuple<int, int>(a, b);
+        }
+
+        private string GetText(Tuple<int, int> a)
+        {
+            return _textArray[a.Item1 + a.Item2];
+        }
+
+        private void PrintValue(string text)
+        {
+            Console.WriteLine("{0}", text);
+        }
+
+        #endregion
+    }
+}
