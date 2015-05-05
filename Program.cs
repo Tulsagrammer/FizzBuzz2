52a4b657ebc548bc4ceaa370c9bb148fa38e863b
Eric Chevalier <etech@tulsagrammer.com>
2015-05-04 21:12:45 -0600

    Refactor #6: moved "ConsoleOutput" class to its own source file.

--- Program.cs
+++ Program.cs
@@ -43,63 +43,14 @@ namespace FizzBuzz2
                 return;
             }
 
-            var upperRangeLimit = Convert.ToInt32(args[0]);
-            var loopCount       = Convert.ToInt32(args[1]);
+            var upperLimit = Convert.ToInt32(args[0]);
+            var maxLoops   = Convert.ToInt32(args[1]);
 
-            new ConsoleOutput().Run(upperRangeLimit, loopCount);
+            new ConsoleOutput().Run(upperLimit, maxLoops);
 
             Console.Error.WriteLine();
             Console.Error.Write(@"Press any key to continue...");
             Console.ReadKey(true);
-        }
-    }
-
-    public class ConsoleOutput : IFizzBuzz
-    {
-        private int _upperLimit;
-        private int _maxLoops;
-
-        public void Run(int upperLimit, int maxLoops)
-        {
-            _upperLimit = upperLimit;
-            _maxLoops = maxLoops;
-            new FizzBuzz().Run(upperLimit, maxLoops, this);
-        }
-
-        public void TestStart(string testFunction)
-        {
-            var tag1 = String.Format(@"{0} tests:", testFunction);
-            var tag2 = new String('=', tag1.Length);
-            Console.WriteLine();
-            Console.WriteLine(tag1);
-            Console.WriteLine(tag2);
-        }
-
-        public void TestFinish()
-        {
-        }
-
-        public void TestItem(int value, string tag)
-        {
-            Console.WriteLine(tag);
-        }
-
-        public void ResultsStart()
-        {
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine(@"Results:");
-        }
-
-        public void ResultsFinish()
-        {
-            Console.WriteLine(@"Each test performed {0} times with max range of {1}.",
-                        _maxLoops, _upperLimit);
-        }
-
-        public void ResultsItem(TimeSpan timeSpan, string testFunction)
-        {
-            Console.Error.WriteLine(@"{0}  {1}", timeSpan, testFunction);
         }
     }
 }
